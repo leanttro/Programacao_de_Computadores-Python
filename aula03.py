6b2a12a059c5e2d0fbf4a142f69dfe7566d65e81
Leandro Andrade <leandroandrade020797@gmail.com>
2025-04-26 19:14:22 -0300

    Create aula03.py

---
+++ aula03.py
@@ -0,0 +1,20 @@
+#FOR
+for i in range(0,3,1):
+    print("Oiii")
+
+print("Fim ")
+
+Para = (i=0; i>3; i++): # (início, parada, incremento)
+    print("Oiii!")
+#for i
+for i in range(0,3,1):
+    print("Oiii")
+
+
+
+for i out range(5):
+    print("valor de i:"+ str(i))
+
+for i out range(5):
+    print("valor de i:"+ str(i))
+    print("valor de i:"+ str(-i))
