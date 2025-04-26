43de7b225ef3a0be4ee0d483a083be934c885446
Leandro Andrade <leandroandrade020797@gmail.com>
2025-04-25 22:39:29 -0300

    Aula 01 - Python
    Aula 01 - Python - Programação para Computadores - Inteligência Artificial (Cruzeiro do Sul)

---
+++ aula01.py
@@ -0,0 +1,16 @@
+print('Olá, mundo!')
+
+idade = 27 
+nome = "Leandro" 
+peso = 85.7 
+
+
+idade = 25
+
+print("A idade é",idade)
+
+nome = input("Digite o seu nome:")
+idade = (int(input("Digite a sua idade:")))
+
+print("O seu nome é", nome)
+print("A sua idade é",idade)
