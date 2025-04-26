938134b45ba2570457c388a30e2fbefb8c6bfbec
Leandro Andrade <leandroandrade020797@gmail.com>
2025-04-26 09:34:14 -0300

    adicionei os comentários na aula 1

--- aula01.py
+++ aula01.py
@@ -1,16 +1,21 @@
-print('Olá, mundo!')
+print('Olá, mundo!') # Imprime uma imagem 
 
-idade = 27 
-nome = "Leandro" 
-peso = 85.7 
+idade = 27 # número inteiro (int)
+nome = "Leandro" # texto (string)
+peso = 85.7 # número quebrado (float)
+# casado = false # verdadeiro ou falso (bool)
 
+# Variáveis com valores definidos (int, string, float) - bool (true/false)
 
 idade = 25
 
-print("A idade é",idade)
+print("A idade é",idade) 
 
-nome = input("Digite o seu nome:")
-idade = (int(input("Digite a sua idade:")))
+# Imprime a frase entre aspas e a última variável definida
+
+nome = input("Digite o seu nome:") # Solicita uma variável do usuário
+idade = (int(input("Digite a sua idade:"))) 
+# Solicita uma variável do usuário e define que é número inteiro.
 
 print("O seu nome é", nome)
 print("A sua idade é",idade)
