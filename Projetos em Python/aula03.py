368030899469a6ebf761e5c614ed2035251e462d
Leandro Andrade <leandroandrade020797@gmail.com>
2025-04-26 21:46:41 -0300

    Update aula03.py

--- Projetos em Python/aula03.py
+++ Projetos em Python/aula03.py
@@ -1,5 +1,69 @@
-for i out range(5):
-    print("valor de i:"+ str(i))
+valor = 0
+for i in range(3):
+    valor = float(input("Digite um número: "))
+    print(valor)
+    print("Fim")
 
-for i out range(5):
-    print("valor de i:"+ str(i))
+sexo = ""
+idade = 0
+quantMulheres = 0
+quantHomens = 0
+
+for i in range(0,5):
+    sexo = input("Digite o sexo (M) ou (H)")
+    idade = int(input("Digite a idade: "))
+
+    if(sexo=="M" or sexo=="m"):
+         quantMulheres+=1
+    elif(sexo=="H" or sexo=="h"):
+         quantHomens+=1
+    
+print("A quantidade de mulheres é", quantMulheres)
+print("A quantidade de homens é", quantHomens)
+
+maiornota = 0
+contador = 0
+nota = 0
+somanotas = 0
+
+while contador < 3:
+     nota = float(input("Digita a nota: "))
+     somanotas = somanotas + nota
+
+     contador+=1
+
+     if(contador==1):
+            maiornota = nota
+     else:
+          if(nota>maiornota):
+            maiornota = nota
+    
+media = somanotas / contador
+     
+print("A maior nota é ",maiornota)
+print("A média de notas é",media)
+print("Fim")
+
+sexo = ""
+idade = 0
+quantMulheres = 0
+valoridade = 0
+
+for i in range(0,5):
+    sexo = input("Digite o sexo (M) ou (H)")
+    idade = int(input("Digite a idade: "))
+
+    if(sexo =="M" or sexo =="m"):
+        quantMulheres+=1
+    
+    valoridade+= idade
+
+mediaidade = valoridade/quantMulheres
+
+print("A quantidade de mulheres é ", quantMulheres)
+print("A média de idade é", mediaidade)
+
+
+
+
+     
